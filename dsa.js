8a0841612f755b3dece93f0395c32d9fa8b58df3
Nawin chander <nawinnapzy289@gmail.com>
2026-06-27 20:28:34 +0530

    add

--- dsa.js
+++ dsa.js
@@ -47,3 +47,33 @@ class TrieNode {
     }
 }
 
+
+// LRU Cache
+
+class LRUCache {
+
+    constructor(capacity) {
+        this.capacity = capacity;
+        this.cache = new Map();
+    }
+
+    get(key) {
+
+        if (!this.cache.has(key)) {
+            return -1;
+        }
+
+        const value =
+            this.cache.get(key);
+
+        this.cache.delete(key);
+
+        this.cache.set(key, value);
+
+        return value;
+    }
+}
+
+
+
+
