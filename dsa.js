d4c58cd0196fd992db2b7ddac0f154070c1dec63
Nawin chander <nawinnapzy289@gmail.com>
2026-06-27 20:27:57 +0530

    add

--- dsa.js
+++ dsa.js
@@ -20,3 +20,30 @@ class PriorityQueue {
     }
 }
 
+
+
+// Interview Question
+// Why Priority Queue over sorting every request?
+
+// Sorting every request costs:
+
+// O(n log n)
+
+// Priority Queue operations cost:
+
+// Insert -> O(log n)
+
+// Remove -> O(log n)
+
+// This becomes extremely important when there are tens of thousands of active drivers.
+
+
+// Trie
+
+class TrieNode {
+    constructor() {
+        this.children = {};
+        this.end = false;
+    }
+}
+
