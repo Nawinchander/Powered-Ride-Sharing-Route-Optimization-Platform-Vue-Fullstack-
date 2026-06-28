d53a0c0f0cc10967c419a3aeda6537d6507a01b1
Nawin chander <nawinnapzy289@gmail.com>
2026-06-28 21:02:06 +0530

    add

---
+++ backend/src/utils/jwt.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+exports.generateAccessToken =
+(payload) => {
+    return jwt.sign(
+        payload,
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "15m"
+        }
+    );
+};
+
+exports.generateRefreshToken =
+(payload) => {
+    return jwt.sign(
+        payload,
+        process.env.JWT_REFRESH_SECRET,
+        {
+            expiresIn: "7d"
+        }
+    );
+};
