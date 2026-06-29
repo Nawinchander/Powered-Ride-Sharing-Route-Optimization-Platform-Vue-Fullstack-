34fd764791b13db3a35189e138b1f3c4ba19db42
Nawin chander <nawinnapzy289@gmail.com>
2026-06-29 17:52:01 +0530

    add

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,49 +1,107 @@
-const bcrypt = require("bcryptjs");
-const jwt = require("jsonwebtoken");
+const User =
+require("../models/User");
 
-exports.register = async (req, res) => {
+const jwtUtil =
+require("../utils/jwt");
 
-    const { name, email, password } = req.body;
+exports.login =
+async (req, res) => {
 
-    const hashedPassword =
-        await bcrypt.hash(password, 10);
-
-    const user = await User.create({
-        name,
+    const {
         email,
-        password: hashedPassword
+        password
+    } = req.body;
+
+    const user =
+    await User.findOne({
+        email
     });
 
-    res.status(201).json(user);
-};
-
-exports.login = async (req, res) => {
-
-    const user = await User.findOne({
-        email: req.body.email
-    });
-
-    const valid =
-        await bcrypt.compare(
-            req.body.password,
-            user.password
-        );
-
-    if (!valid) {
-        return res.status(401).json({
-            message: "Invalid Credentials"
+    if (!user) {
+        return res.status(404)
+        .json({
+            message:
+            "User not found"
         });
     }
 
-    const token = jwt.sign(
-        {
-            id: user._id,
-            role: user.role
-        },
-        process.env.JWT_SECRET
+    const valid =
+    await bcrypt.compare(
+        password,
+        user.password
     );
 
-    res.json({ token });
+    if (!valid) {
+        return res.status(401)
+        .json({
+            message:
+            "Invalid Password"
+        });
+    }
+
+    const accessToken =
+    jwtUtil.generateAccessToken({
+        id: user._id,
+        role: user.role
+    });
+
+    const refreshToken =
+    jwtUtil.generateRefreshToken({
+        id: user._id
+    });
+
+    res.json({
+        accessToken,
+        refreshToken
+    });
 };
+
+// const bcrypt = require("bcryptjs");
+// const jwt = require("jsonwebtoken");
+
+// exports.register = async (req, res) => {
+
+//     const { name, email, password } = req.body;
+
+//     const hashedPassword =
+//         await bcrypt.hash(password, 10);
+
+//     const user = await User.create({
+//         name,
+//         email,
+//         password: hashedPassword
+//     });
+
+//     res.status(201).json(user);
+// };
+
+// exports.login = async (req, res) => {
+
+//     const user = await User.findOne({
+//         email: req.body.email
+//     });
+
+//     const valid =
+//         await bcrypt.compare(
+//             req.body.password,
+//             user.password
+//         );
+
+//     if (!valid) {
+//         return res.status(401).json({
+//             message: "Invalid Credentials"
+//         });
+//     }
+
+//     const token = jwt.sign(
+//         {
+//             id: user._id,
+//             role: user.role
+//         },
+//         process.env.JWT_SECRET
+//     );
+
+//     res.json({ token });
+// };
 
 
