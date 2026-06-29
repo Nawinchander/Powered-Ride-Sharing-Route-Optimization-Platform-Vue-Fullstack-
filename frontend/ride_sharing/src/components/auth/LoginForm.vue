af06045f22838e6bb7e97100021163639e5bca9d
Nawin chander <nawinnapzy289@gmail.com>
2026-06-29 17:55:19 +0530

    add

---
+++ frontend/ride_sharing/src/components/auth/LoginForm.vue
@@ -0,0 +1,51 @@
+<template>
+
+<form @submit.prevent="login">
+
+<input
+v-model="email"
+placeholder="Email"
+/>
+
+<input
+type="password"
+v-model="password"
+placeholder="Password"
+/>
+
+<button>
+Login
+</button>
+
+</form>
+
+</template>
+
+<script setup>
+
+import { ref }
+from "vue";
+
+import {
+useAuthStore
+}
+from "../../stores/auth.store";
+
+const email = ref("");
+const password = ref("");
+
+const store =
+useAuthStore();
+
+const login =
+async () => {
+
+    await store.login(
+        email.value,
+        password.value
+    );
+};
+
+</script>
+
+
