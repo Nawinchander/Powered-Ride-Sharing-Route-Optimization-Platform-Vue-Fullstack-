95b2b235284c3655660175abef49c16cc5cd70b2
Nawin chander <nawinnapzy289@gmail.com>
2026-06-29 17:53:04 +0530

    add

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,18 +1,34 @@
-const router = require("express").Router();
+const router =
+require("express").Router();
 
-const authController =
-require("../controllers/auth.controller");
-
-router.post(
-    "/register",
-    authController.register
+const controller =
+require(
+"../controllers/auth.controller"
 );
 
 router.post(
     "/login",
-    authController.login
+    controller.login
 );
 
-module.exports = router;
+module.exports =
+router;
+
+// const router = require("express").Router();
+
+// const authController =
+// require("../controllers/auth.controller");
+
+// router.post(
+//     "/register",
+//     authController.register
+// );
+
+// router.post(
+//     "/login",
+//     authController.login
+// );
+
+// module.exports = router;
 
 
