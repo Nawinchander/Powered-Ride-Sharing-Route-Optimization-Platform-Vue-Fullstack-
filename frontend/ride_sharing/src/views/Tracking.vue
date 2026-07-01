b0ae43bf8905c5f3879a6a87fb98e537996ec6ac
Nawin chander <nawinnapzy289@gmail.com>
2026-07-01 18:26:57 +0530

    add

---
+++ frontend/ride_sharing/src/views/Tracking.vue
@@ -0,0 +1,64 @@
+<template>
+
+<div>
+
+<DriverMarker
+v-for="
+driver
+in store.drivers
+"
+:key="
+driver.driverId
+"
+:driver="driver"
+/>
+
+</div>
+
+</template>
+
+<script setup>
+
+import DriverMarker
+from
+"../components/tracking/DriverMarker.vue";
+
+import {
+onMounted
+}
+from "vue";
+
+import useSocket
+from
+"../composables/useSocket";
+
+import {
+useTrackingStore
+}
+from
+"../stores/tracking.store";
+
+const socket =
+useSocket();
+
+const store =
+useTrackingStore();
+
+onMounted(() => {
+
+socket.on(
+"location-updated",
+(location) => {
+
+store.updateDriver(
+location
+);
+
+});
+
+});
+
+</script>
+
+
+
