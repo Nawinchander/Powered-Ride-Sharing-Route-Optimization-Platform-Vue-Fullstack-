cc7cebf8a65c39cdf8b5ec6143807568d9b35a80
Nawin chander <nawinnapzy289@gmail.com>
2026-07-02 19:51:08 +0530

    add

---
+++ backend/src/controllers/ride.controller.js
@@ -0,0 +1,48 @@
+const rideService =
+require(
+"../services/ride.service"
+);
+
+exports.createRide =
+async (
+req,
+res
+) => {
+
+try {
+
+const ride =
+await rideService
+.createRide({
+    riderId:
+    req.user.id,
+
+    pickupLocation:
+    req.body.pickupLocation,
+
+    destinationLocation:
+    req.body.destinationLocation,
+
+    fare:
+    req.body.fare,
+
+    distance:
+    req.body.distance
+});
+
+res.status(201)
+.json(ride);
+
+}
+catch(error){
+    res.status(500)
+    .json({
+        message:
+        error.message
+    });
+}
+
+};
+
+
+
