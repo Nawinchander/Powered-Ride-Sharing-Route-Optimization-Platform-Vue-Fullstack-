c962127a0ce20d3fbcb5003609b0c4abf592a5f1
Nawin chander <nawinnapzy289@gmail.com>
2026-07-02 19:41:55 +0530

    add

---
+++ backend/src/services/ride.service.js
@@ -0,0 +1,36 @@
+const rideRepository =
+require(
+"../repositories/ride.repository"
+);
+
+exports.createRide =
+async (
+rideData
+) => {
+
+    const ride =
+    await rideRepository
+    .createRide({
+        ...rideData,
+        status: "REQUESTED"
+    });
+
+    return ride;
+};
+
+exports.assignDriver =
+async (
+rideId,
+driverId
+) => {
+
+    return rideRepository
+    .updateRide(
+        rideId,
+        {
+            driverId,
+            status:
+            "DRIVER_ASSIGNED"
+        }
+    );
+};
