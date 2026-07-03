a8a1bbe80b6e2452a505acfb66e98cf6493f74dd
Nawin chander <nawinnapzy289@gmail.com>
2026-07-03 19:29:54 +0530

    add

---
+++ frontend/ride_sharing/src/components/ride/ride.form.vue
@@ -0,0 +1,56 @@
+<template>
+
+<div>
+
+<input
+v-model="pickup"
+placeholder="Pickup"
+/>
+
+<input
+v-model="destination"
+placeholder="Destination"
+/>
+
+<button @click="bookRide" Book Ride </button>
+
+</div>
+
+</template>
+
+<script setup>
+
+import { ref } from "vue";
+
+import { useRideStore } from "../../stores/ride.store";
+
+const pickup = ref("");
+
+const destination = ref("");
+
+const store = useRideStore();
+
+const bookRide = async () => {
+
+await store.createRide({
+
+pickupLocation: {
+    address:
+    pickup.value
+},
+
+destinationLocation: {
+    address:
+    destination.value
+},
+
+fare: 250,
+
+distance: 8
+
+});
+
+};
+
+</script>
+
