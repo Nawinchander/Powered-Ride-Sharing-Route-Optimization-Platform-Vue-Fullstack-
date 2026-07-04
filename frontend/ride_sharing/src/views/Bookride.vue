62f750be233ffc44a007a1485ac5754b96ce66e9
Nawin chander <nawinnapzy289@gmail.com>
2026-07-04 13:15:49 +0530

    add

---
+++ frontend/ride_sharing/src/views/Bookride.vue
@@ -0,0 +1,23 @@
+<template>
+
+<div>
+
+<h1>
+Book Ride
+</h1>
+
+<RideForm />
+
+</div>
+
+</template>
+
+<script setup>
+
+import RideForm from "../components/ride/RideForm.vue";
+
+
+</script>
+
+
+
