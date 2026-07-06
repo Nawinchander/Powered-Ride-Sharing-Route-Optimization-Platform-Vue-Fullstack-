001f63fa8c1626d83af8dd6fb56531ff04e173ed
Nawin chander <nawinnapzy289@gmail.com>
2026-07-06 12:18:31 +0530

    add

---
+++ backend/src/services/eta.service.js
@@ -0,0 +1,25 @@
+const dijkstra =
+require(
+    "../algorithms/Dijkstra"
+);
+
+exports.calculateETA =
+(
+    graph,
+    source,
+    destination
+) => {
+
+    const distance =
+        dijkstra(
+            graph,
+            source,
+            destination
+        );
+
+    const averageSpeed = 30;
+
+    return Math.ceil(
+        distance / averageSpeed * 60
+    );
+};
