e16220bea13444dd62a1fe6d015b1a7e632e7eed
Nawin chander <nawinnapzy289@gmail.com>
2026-07-06 12:34:35 +0530

    add

---
+++ frontend/ride_sharing/src/components/pricing/FareEstimate.vue
@@ -0,0 +1,24 @@
+<template>
+
+<div>
+
+<h3> Estimated Fare </h3>
+
+<h1> ₹ {{ pricingStore.fare }} </h1>
+
+<p> Surge: {{ pricingStore.multiplier }}x </p>
+
+</div>
+
+</template>
+
+<script setup>
+
+import { usePricingStore } from "../../stores/pricing.store";
+
+const pricingStore = usePricingStore();
+
+</script>
+
+
+
