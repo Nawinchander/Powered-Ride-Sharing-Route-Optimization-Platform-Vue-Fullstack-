af6f8f90e3c31882a4ce4fd9499e187d43c98c1a
Nawin chander <nawinnapzy289@gmail.com>
2026-07-09 17:34:10 +0530

    add

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -1,15 +1,19 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema(
-{
-    name: String,
+const userSchema = new mongoose.Schema({
+
+    name: {
+        type: String
+    },
 
     email: {
         type: String,
         unique: true
     },
 
-    password: String,
+    password: {
+        type: String
+    },
 
     role: {
         type: String,
@@ -17,16 +21,49 @@ const userSchema = new mongoose.Schema(
             "RIDER",
             "DRIVER",
             "ADMIN"
-        ]
+        ],
+        default: "RIDER"
     }
-},
-{
-    timestamps: true
-}
-);
 
-module.exports =
-mongoose.model(
+},{
+    timestamps:true
+});
+
+module.exports = mongoose.model(
     "User",
     userSchema
 );
+
+
+// const mongoose = require("mongoose");
+
+// const userSchema = new mongoose.Schema(
+// {
+//     name: String,
+
+//     email: {
+//         type: String,
+//         unique: true
+//     },
+
+//     password: String,
+
+//     role: {
+//         type: String,
+//         enum: [
+//             "RIDER",
+//             "DRIVER",
+//             "ADMIN"
+//         ]
+//     }
+// },
+// {
+//     timestamps: true
+// }
+// );
+
+// module.exports =
+// mongoose.model(
+//     "User",
+//     userSchema
+// );
