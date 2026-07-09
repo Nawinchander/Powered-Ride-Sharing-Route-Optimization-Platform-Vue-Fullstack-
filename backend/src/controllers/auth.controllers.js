461c9834935022bf3253c5e4f0a03671f81bce64
Nawin chander <nawinnapzy289@gmail.com>
2026-07-09 17:35:07 +0530

    add

--- backend/src/controllers/auth.controllers.js
+++ backend/src/controllers/auth.controllers.js
@@ -1,107 +1,182 @@
-const User =
-require("../models/User");
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-const jwtUtil =
-require("../utils/jwt");
+exports.login = async (req, res) => {
 
-exports.login =
-async (req, res) => {
+    try{
 
-    const {
-        email,
-        password
-    } = req.body;
+        const { email, password } = req.body;
 
-    const user =
-    await User.findOne({
-        email
-    });
-
-    if (!user) {
-        return res.status(404)
-        .json({
-            message:
-            "User not found"
+        const user = await User.findOne({
+            email
         });
+
+        if(!user){
+            return res.status(404).json({
+                message:"User not found"
+            });
+        }
+
+        const validPassword =
+        await bcrypt.compare(
+            password,
+            user.password
+        );
+
+        if(!validPassword){
+
+            return res.status(401).json({
+                message:"Invalid Password"
+            });
+
+        }
+
+        const token = jwt.sign(
+
+            {
+                id:user._id,
+                role:user.role
+            },
+
+            process.env.JWT_SECRET,
+
+            {
+                expiresIn:"1d"
+            }
+
+        );
+
+        res.status(200).json({
+
+            token,
+
+            user:{
+                id:user._id,
+                name:user.name,
+                email:user.email,
+                role:user.role
+            }
+
+        });
+
     }
 
-    const valid =
-    await bcrypt.compare(
-        password,
-        user.password
-    );
+    catch(error){
 
-    if (!valid) {
-        return res.status(401)
-        .json({
-            message:
-            "Invalid Password"
+        res.status(500).json({
+            message:error.message
         });
+
     }
 
-    const accessToken =
-    jwtUtil.generateAccessToken({
-        id: user._id,
-        role: user.role
-    });
-
-    const refreshToken =
-    jwtUtil.generateRefreshToken({
-        id: user._id
-    });
-
-    res.json({
-        accessToken,
-        refreshToken
-    });
 };
 
-// const bcrypt = require("bcryptjs");
-// const jwt = require("jsonwebtoken");
 
-// exports.register = async (req, res) => {
+// const User =
+// require("../models/User");
 
-//     const { name, email, password } = req.body;
+// const jwtUtil =
+// require("../utils/jwt");
 
-//     const hashedPassword =
-//         await bcrypt.hash(password, 10);
+// exports.login =
+// async (req, res) => {
 
-//     const user = await User.create({
-//         name,
+//     const {
 //         email,
-//         password: hashedPassword
+//         password
+//     } = req.body;
+
+//     const user =
+//     await User.findOne({
+//         email
 //     });
 
-//     res.status(201).json(user);
-// };
-
-// exports.login = async (req, res) => {
-
-//     const user = await User.findOne({
-//         email: req.body.email
-//     });
-
-//     const valid =
-//         await bcrypt.compare(
-//             req.body.password,
-//             user.password
-//         );
-
-//     if (!valid) {
-//         return res.status(401).json({
-//             message: "Invalid Credentials"
+//     if (!user) {
+//         return res.status(404)
+//         .json({
+//             message:
+//             "User not found"
 //         });
 //     }
 
-//     const token = jwt.sign(
-//         {
-//             id: user._id,
-//             role: user.role
-//         },
-//         process.env.JWT_SECRET
+//     const valid =
+//     await bcrypt.compare(
+//         password,
+//         user.password
 //     );
 
-//     res.json({ token });
+//     if (!valid) {
+//         return res.status(401)
+//         .json({
+//             message:
+//             "Invalid Password"
+//         });
+//     }
+
+//     const accessToken =
+//     jwtUtil.generateAccessToken({
+//         id: user._id,
+//         role: user.role
+//     });
+
+//     const refreshToken =
+//     jwtUtil.generateRefreshToken({
+//         id: user._id
+//     });
+
+//     res.json({
+//         accessToken,
+//         refreshToken
+//     });
 // };
+
+// // const bcrypt = require("bcryptjs");
+// // const jwt = require("jsonwebtoken");
+
+// // exports.register = async (req, res) => {
+
+// //     const { name, email, password } = req.body;
+
+// //     const hashedPassword =
+// //         await bcrypt.hash(password, 10);
+
+// //     const user = await User.create({
+// //         name,
+// //         email,
+// //         password: hashedPassword
+// //     });
+
+// //     res.status(201).json(user);
+// // };
+
+// // exports.login = async (req, res) => {
+
+// //     const user = await User.findOne({
+// //         email: req.body.email
+// //     });
+
+// //     const valid =
+// //         await bcrypt.compare(
+// //             req.body.password,
+// //             user.password
+// //         );
+
+// //     if (!valid) {
+// //         return res.status(401).json({
+// //             message: "Invalid Credentials"
+// //         });
+// //     }
+
+// //     const token = jwt.sign(
+// //         {
+// //             id: user._id,
+// //             role: user.role
+// //         },
+// //         process.env.JWT_SECRET
+// //     );
+
+// //     res.json({ token });
+// // };
 
 
