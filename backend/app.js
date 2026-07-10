9c1db7afc1b10540617b529436cc9df4975b2486
Nawin chander <nawinnapzy289@gmail.com>
2026-07-10 17:05:47 +0530

    add

--- backend/app.js
+++ backend/app.js
@@ -2,21 +2,37 @@ const express = require("express");
 const cors = require("cors");
 
 const authRoutes = require("./routes/auth.routes");
-const rideRoutes = require( "./routes/ride.routes" );
 
 const app = express();
 
-const pricingRoutes = require( "./routes/pricing.routes" );
-
-
 app.use(cors());
-app.use(express.json());
-app.use( "/api/rides", rideRoutes );
 
-app.use("/api/pricing", pricingRoutes );
-app.use("/api/auth", authRoutes);
+app.use(express.json());
+
+app.use("/api/auth",authRoutes);
 
 module.exports = app;
+
+
+// const express = require("express");
+// const cors = require("cors");
+
+// const authRoutes = require("./routes/auth.routes");
+// const rideRoutes = require( "./routes/ride.routes" );
+
+// const app = express();
+
+// const pricingRoutes = require( "./routes/pricing.routes" );
+
+
+// app.use(cors());
+// app.use(express.json());
+// app.use( "/api/rides", rideRoutes );
+
+// app.use("/api/pricing", pricingRoutes );
+// app.use("/api/auth", authRoutes);
+
+// module.exports = app;
 
 
 
