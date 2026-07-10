7b6c52df79e2e460ea620d7b8a804e30e2179006
Nawin chander <nawinnapzy289@gmail.com>
2026-07-10 17:08:12 +0530

    ad

--- frontend/ride_sharing/src/api/auth.api.js
+++ frontend/ride_sharing/src/api/auth.api.js
@@ -1,5 +1,14 @@
+// import axios from "axios";
+
+// export default axios.create({
+//     baseURL: "http://localhost:5000/api"
+// });
+
+
+
 import axios from "axios";
 
 export default axios.create({
     baseURL: "http://localhost:5000/api"
 });
+
