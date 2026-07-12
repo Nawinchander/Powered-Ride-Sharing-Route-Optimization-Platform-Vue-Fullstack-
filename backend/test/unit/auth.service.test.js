6675798585afc3194beba43aff61bd4305cee83c
Nawin chander <nawinnapzy289@gmail.com>
2026-07-12 17:54:52 +0530

    add

---
+++ backend/test/unit/auth.service.test.js
@@ -0,0 +1,11 @@
+describe("Authentication Service", () => {
+
+    test("Password should match", () => {
+
+        const password = "123456";
+
+        expect(password).toBe("123456");
+
+    });
+
+});
