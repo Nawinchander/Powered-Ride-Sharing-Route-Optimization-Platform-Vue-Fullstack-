125db67b24024446b60e25aaf7425d77286d9a43
Nawin chander <nawinnapzy289@gmail.com>
2026-07-13 17:02:11 +0530

    add

---
+++ backend/test/unit/priorityQueue.test.js
@@ -0,0 +1,23 @@
+const PriorityQueue =
+require("../src/algorithms/PriorityQueue");
+
+describe("Priority Queue", () => {
+
+test("Insert Driver", () => {
+
+const queue =
+new PriorityQueue();
+
+queue.insert({
+
+score:2
+
+});
+
+expect(
+queue.heap.length
+).toBe(1);
+
+});
+
+});
