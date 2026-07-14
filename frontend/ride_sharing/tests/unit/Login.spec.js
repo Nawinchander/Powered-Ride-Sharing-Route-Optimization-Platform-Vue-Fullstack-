0641720a064fe1fff7f5ba28110dceaeee7f16df
Nawin chander <nawinnapzy289@gmail.com>
2026-07-14 20:23:09 +0530

    add

---
+++ frontend/ride_sharing/tests/unit/Login.spec.js
@@ -0,0 +1,24 @@
+import {
+
+describe,
+
+test,
+
+expect
+
+}
+
+from "vitest";
+
+describe("Login",()=>{
+
+test("Should Login",()=>{
+
+const user = true;
+
+expect(user)
+.toBe(true);
+
+});
+
+});
