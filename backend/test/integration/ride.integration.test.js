b92b45f1a48ad77cfd354bf9508f0a3d87f3afc6
Nawin chander <nawinnapzy289@gmail.com>
2026-07-14 20:04:39 +0530

    add

---
+++ backend/test/integration/ride.integration.test.js
@@ -0,0 +1,31 @@
+const request =
+require("supertest");
+
+const app =
+require("../src/app");
+
+describe("Ride API",()=>{
+
+test("Create Ride",async()=>{
+
+const response =
+await request(app)
+
+.post("/api/rides")
+
+.send({
+
+pickup:"A",
+
+destination:"B"
+
+});
+
+expect(response.status)
+.toBeDefined();
+
+});
+
+});
+
+
