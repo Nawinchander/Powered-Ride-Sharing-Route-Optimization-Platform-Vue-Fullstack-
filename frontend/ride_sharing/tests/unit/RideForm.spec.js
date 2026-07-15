59ed9bb1f3c9daee13d34d4666198b2e9907d6db
Nawin chander <nawinnapzy289@gmail.com>
2026-07-15 15:22:01 +0530

    add

--- frontend/ride_sharing/tests/unit/RideForm.spec.js
+++ frontend/ride_sharing/tests/unit/RideForm.spec.js
@@ -1,1 +1,28 @@
 /// rider form
+
+import {
+
+describe,
+
+test,
+
+expect
+
+}
+
+from "vitest";
+
+describe("Ride Form",()=>{
+
+test("Pickup Exists",()=>{
+
+const pickup = "Airport";
+
+expect(pickup)
+.toBe("Airport");
+
+});
+
+});
+
+
