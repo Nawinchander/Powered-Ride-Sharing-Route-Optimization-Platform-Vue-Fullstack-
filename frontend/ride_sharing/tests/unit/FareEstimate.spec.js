0e55c9444988f58340d5c3fa843e402c328d5b24
Nawin chander <nawinnapzy289@gmail.com>
2026-07-15 15:25:01 +0530

    add

--- frontend/ride_sharing/tests/unit/FareEstimate.spec.js
+++ frontend/ride_sharing/tests/unit/FareEstimate.spec.js
@@ -1,3 +1,29 @@
 /// fare estimate spec
 
 
+import {
+
+describe,
+
+expect,
+
+test
+
+}
+
+from "vitest";
+
+describe("Fare",()=>{
+
+test("Fare Greater",()=>{
+
+const fare=250;
+
+expect(fare)
+.toBeGreaterThan(100);
+
+});
+
+});
+
+
