2842f4352cea1f5a5366c7c36a778fca53012aeb
Nawin chander <nawinnapzy289@gmail.com>
2026-07-16 17:55:51 +0530

    add

---
+++ frontend/ride_sharing/tests/unit/DriverCard.spec.js
@@ -0,0 +1,24 @@
+import {
+
+describe,
+
+test,
+
+expect
+
+}
+
+from "vitest";
+
+describe("Driver Card",()=>{
+
+test("Driver Name",()=>{
+
+const name="John";
+
+expect(name)
+.toBe("John");
+
+});
+
+});
