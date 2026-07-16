4f9d5cf1ef01b4ad34f2f6df40da574d1d1746a2
Nawin chander <nawinnapzy289@gmail.com>
2026-07-16 17:58:29 +0530

    add

--- frontend/ride_sharing/tests/integration/BookingFlow.spec.js
+++ frontend/ride_sharing/tests/integration/BookingFlow.spec.js
@@ -1,1 +1,17 @@
 /// booking flow
+
+import {describe,expect,test}from "vitest";
+
+describe("Booking",()=>{
+
+test("Booking Success",()=>{
+
+const booked=true;
+
+expect(booked).toBe(true);
+
+});
+
+});
+
+
