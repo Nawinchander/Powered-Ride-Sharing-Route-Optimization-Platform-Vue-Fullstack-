4cc4f147daad3bd4934beb19bde95afae0f1768c
Nawin chander <nawinnapzy289@gmail.com>
2026-07-17 19:53:30 +0530

    add

--- frontend/ride_sharing/tests/e2e/login.cy.js
+++ frontend/ride_sharing/tests/e2e/login.cy.js
@@ -1,2 +1,28 @@
 /// login cy
 
+describe("Login Page",()=>{
+
+it("Login",()=>{
+
+cy.visit("/");
+
+cy.get("input")
+.first()
+
+.type("test@test.com");
+
+cy.get(
+"input[type=password]"
+)
+
+.type("123456");
+
+cy.contains("Login")
+.click();
+
+});
+
+});
+
+
+
