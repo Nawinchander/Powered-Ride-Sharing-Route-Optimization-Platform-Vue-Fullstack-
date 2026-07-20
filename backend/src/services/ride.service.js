ef071ea9096126e56a78a256ca96e8e6ad5b4a8b
Nawin chander <nawinnapzy289@gmail.com>
2026-07-20 19:34:38 +0530

    add

--- backend/src/services/ride.service.js
+++ backend/src/services/ride.service.js
@@ -1,4 +1,32 @@
 const rideRepository = require("../repositories/ride.repository");
+const Ride = require("../models/Ride")
+
+const rides = [];
+
+
+// Create Ride
+exports.createRide = (pickup, destination) => {
+
+    const ride = new Ride(
+        rides.length + 1,
+        pickup,
+        destination
+    );
+
+    rides.push(ride);
+
+    return ride;
+};
+
+// Get All Rides
+exports.getRides = () => {
+
+    return rides;
+
+};
+
+
+
 
 exports.createRide = async (rideData) => {
 
