c6663ff9c5f0e5008611340f0427a50508f66593
Nawin chander <nawinnapzy289@gmail.com>
2026-07-20 19:36:15 +0530

    add

--- backend/src/controllers/ride.controller.js
+++ backend/src/controllers/ride.controller.js
@@ -1,44 +1,21 @@
-const rideService =
-require(
-"../services/ride.service"
-);
+const rideService = require("../services/ride.service");
 
-exports.createRide =
-async (
-req,
-res
-) => {
+exports.createRide = async ( req, res) => {
 
-try {
-
-const ride =
-await rideService
-.createRide({
-    riderId:
-    req.user.id,
-
-    pickupLocation:
-    req.body.pickupLocation,
-
-    destinationLocation:
-    req.body.destinationLocation,
-
-    fare:
-    req.body.fare,
-
-    distance:
-    req.body.distance
+try { const ride = await rideService.createRide({
+    riderId: req.user.id,
+    pickupLocation: req.body.pickupLocation,
+    destinationLocation: req.body.destinationLocation,
+    fare: req.body.fare,
+    distance:req.body.distance
 });
 
-res.status(201)
-.json(ride);
+res.status(201).json(ride);
 
 }
 catch(error){
-    res.status(500)
-    .json({
-        message:
-        error.message
+    res.status(500).json({
+        message: error.message
     });
 }
 
