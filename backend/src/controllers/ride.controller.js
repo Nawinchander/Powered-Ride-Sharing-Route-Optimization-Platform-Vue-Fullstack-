eb8e1b3e91b97708c061a54a1bf6b50afdf5750e
Nawin chander <nawinnapzy289@gmail.com>
2026-07-20 19:38:20 +0530

    add

--- backend/src/controllers/ride.controller.js
+++ backend/src/controllers/ride.controller.js
@@ -8,11 +8,34 @@ try { const ride = await rideService.createRide({
     destinationLocation: req.body.destinationLocation,
     fare: req.body.fare,
     distance:req.body.distance
-});
+
+})
+
+// Create Ride
+exports.createRide = (req, res) => {
+
+    const { pickup, destination } = req.body;
+
+    const ride = rideService.createRide( pickup, destination );
+
+    res.json(ride);
+
+};
 
 res.status(201).json(ride);
 
 }
+
+// Get All Rides
+exports.getRides = (req, res) => {
+
+    res.json(
+        rideService.getRides()
+    );
+
+};
+
+
 catch(error){
     res.status(500).json({
         message: error.message
