bc814b36497560581c9155bde2cfc8e283d1fc30
Nawin chander <nawinnapzy289@gmail.com>
2026-07-21 17:53:27 +0530

    add

--- backend/src/routes/ride.routes.js
+++ backend/src/routes/ride.routes.js
@@ -6,6 +6,19 @@ const auth = require( "../middleware/auth.middleware" );
 
 router.post( "/", auth, controller.createRide );
 
+
+// Create Ride
+router.post(
+    "/",
+    rideController.createRide
+);
+
+// Get Rides
+router.get(
+    "/",
+    rideController.getRides
+);
+
 module.exports = router;
 
 
