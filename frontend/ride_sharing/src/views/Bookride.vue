771cb13fd2f16b4fe357f7a97bb2cc2939469c9a
Nawin chander <nawinnapzy289@gmail.com>
2026-07-21 18:07:19 +0530

    add

--- frontend/ride_sharing/src/views/Bookride.vue
+++ frontend/ride_sharing/src/views/Bookride.vue
@@ -4,6 +4,18 @@
 
 <h1> Book Ride </h1>
 
+<input
+v-model="pickup"
+placeholder="Pickup"
+/>
+
+<input
+v-model="destination"
+placeholder="Destination"
+/>
+
+<button @click="bookRide">Book</button>
+
 <RideForm />
 
 </div>
