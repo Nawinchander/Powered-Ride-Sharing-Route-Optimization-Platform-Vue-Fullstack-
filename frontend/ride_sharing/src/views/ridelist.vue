aa9e58b9cdbf872b5420c52f4ebaa86f9f77cf57
Nawin chander <nawinnapzy289@gmail.com>
2026-07-22 19:56:06 +0530

    add

--- frontend/ride_sharing/src/views/ridelist.vue
+++ frontend/ride_sharing/src/views/ridelist.vue
@@ -1,2 +1,54 @@
 ///// ride list
 
+<template>
+
+<div>
+
+<h2>Ride List</h2>
+
+<button
+@click="rideStore.getRides()"
+>
+
+Load Rides
+
+</button>
+
+<ul>
+
+<li
+v-for="ride in rideStore.rides"
+:key="ride.id"
+>
+
+{{ ride.pickup }}
+
+➡️
+
+{{ ride.destination }}
+
+-
+
+{{ ride.status }}
+
+</li>
+
+</ul>
+
+</div>
+
+</template>
+
+<script setup>
+
+import {
+useRideStore
+}
+from "../stores/ride.store";
+
+const rideStore =
+useRideStore();
+
+</script>
+
+
